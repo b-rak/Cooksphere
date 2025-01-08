04389c0eb2c7fad6ca3c6aa90e73c8f290cc75c1
Burak Tas <b.tas93@googlemail.com>
2025-01-08 17:24:23 +0100

    add cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 'use strict';
+import cors from 'cors';
 import express from 'express';
 import router from './router.js'
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors({origin: 'http://localhost:5173'}));
 app.use(express.json());
 app.use(router);
 
