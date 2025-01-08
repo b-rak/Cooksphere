9e54333e94c62eafd89252911dc53d122e59fca9
Burak Tas <b.tas93@googlemail.com>
2025-01-08 15:49:05 +0100

    add mongoose connection

---
+++ server/models/index.js
@@ -0,0 +1,13 @@
+'use strict';
+import mongoose from "mongoose";
+
+(async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/cooksphere");
+    console.log("Connected to MongoDB");
+  } catch (e) {
+    console.log(`MongoDB connection error: ${e}`)
+  }
+})();
+
+export default mongoose;
