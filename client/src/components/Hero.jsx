5c416e7b3e767df97bfb3c5bc0e9ab5729974f1c
Burak Tas <b.tas93@googlemail.com>
2025-01-09 12:23:08 +0100

    add component: Hero

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+export function Hero () {
+  const random = Math.floor(Math.random()*10);
+  const heroURL = `landingpage/hero_${random}.jpg`;
+
+  return (
+    <>
+      <div className='h-[32rem] flex flex-col items-center gap-4 pt-8 bg-cover bg-center bg-no-repeat' style={{ backgroundImage: `url(${heroURL})` }}>
+        <h1>Hero Search</h1>
+        <input type='text' placeholder='Search recipe'></input>
+      </div>
+    </>
+  )
+};
