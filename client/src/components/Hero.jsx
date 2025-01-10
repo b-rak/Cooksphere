bc73739983830287cb5b7101e746caa0f907f356
Burak Tas <b.tas93@googlemail.com>
2025-01-10 17:36:30 +0100

    get hero images from cloud

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,18 @@
 export function Hero () {
   const random = Math.floor(Math.random()*10);
-  const heroURL = `landingpage/hero_${random}.jpg`;
+  const imageIds = [
+    'cyt8zgujhq6olzqphqgb',
+    'g68uhpx7wcdo597dfj5x',
+    'idpol0hfuz1odf3iywyk',
+    'nyjccvchdn13wvgm8aba',
+    'yzchiwqaothmtoqce9to',
+    'krsw6xcyxq79mzoyy1ht',
+    'mjeu6ianbjmbtpy2jvew',
+    'lcwabeyiz0qbhh8gnvrq',
+    'z5pexddrqeiwpasbk7gj',
+    'dp7chefwtxuqqzdult99',
+  ];
+  const heroURL = `https://res.cloudinary.com/drm5qsq0p/image/upload/v1736524856/${imageIds[random]}.jpg`;
 
   return (
     <>
