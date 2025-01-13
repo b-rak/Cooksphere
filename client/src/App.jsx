67cb90e01a82a1d742a4ae0ccda56bde2c6f45af
Burak Tas <b.tas93@googlemail.com>
2025-01-13 10:22:17 +0100

    add AuthContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,21 @@
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import './App.css';
-import { getCategories, getLatestRecipes } from './ApiClient';
+import { getCategories, getLatestRecipes, login } from './ApiClient';
 import { CategoryList } from './components/CategoryList';
 import { Hero } from './components/Hero';
 import { Navbar } from './components/Navbar';
 import { CategoryPage } from './components/CategoryPage';
-import { Recipe } from './components/Recipe';
+import { RecipeDetailsPage } from './components/RecipeDetailsPage';
 import { RecipeList } from './components/RecipeList';
 import { Routes, Route } from "react-router";
 import { Profile } from './components/Profile';
 
+export const AuthContext = createContext(null);
 function App() {
   const [categories, setCategories] = useState([])
   const [latest, setLatest] = useState([])
+
+  const [currentUser, setCurrentUser] = useState({});
 
   useEffect(() => {
     getCategories()
@@ -26,23 +29,31 @@ function App() {
     .catch(e => console.log(e));
   }, []);
 
+  useEffect(() => {
+    login({email: 'zappe.thomson@test.com', password: 'Test123!'})
+      .then(data => setCurrentUser(data))
+      .catch(e => console.log(e));
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Hero />
-            <div className='bg-orange-200 pl-8 py-4'>
-              <CategoryList title={'Recipe Categories'} listItems={categories}/>
-              <RecipeList title={'New Added Recipes'} recipes={latest}/>
-            </div>
-          </>
-        }/>
-        <Route path="/recipe/:recipeId" element={<Recipe />}/>
-        <Route path="/recipes/category/:category" element={<CategoryPage />}/>
-        <Route path="/profile" element={<Profile />}/>
-      </Routes>
+      <AuthContext.Provider value={currentUser}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <div className='bg-orange-200 pl-8 py-4'>
+                <CategoryList title={'Recipe Categories'} listItems={categories}/>
+                <RecipeList title={'New Added Recipes'} recipes={latest}/>
+              </div>
+            </>
+          }/>
+          <Route path="/recipe/:recipeId" element={<RecipeDetailsPage />}/>
+          <Route path="/recipes/category/:category" element={<CategoryPage />}/>
+          <Route path="/profile" element={<Profile />}/>
+        </Routes>
+      </AuthContext.Provider>
     </>
   )
 }
