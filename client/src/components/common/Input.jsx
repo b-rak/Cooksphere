90d868e7f443581dbf60d9894af5c43a22ecfc3c
Burak Tas <b.tas93@googlemail.com>
2025-01-13 10:27:40 +0100

    add error message

--- client/src/components/common/Input.jsx
+++ client/src/components/common/Input.jsx
@@ -1,9 +1,12 @@
-export function Input ({id, name, value, text, handleChange}) {
+export function Input ({id, name, value, text, error, handleChange}) {
   return (
     <>
       <div className="flex flex-col w-96">
         <label htmlFor={id}>{text}</label>
         <input type="text" id={id} name={name} value={value} className="border" onChange={handleChange}/>
+        {error &&
+          <span className="text-red-600">{name[0].toUpperCase() + name.substring(1) + ' is required.'}</span>
+        }
       </div>
     </>
   )
