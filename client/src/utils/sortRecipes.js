6e654b3aade0b78078e78fd5c79e38e38026df56
Burak Tas <b.tas93@googlemail.com>
2025-01-14 13:38:24 +0100

    add sort recipes by rating

--- client/src/utils/sortRecipes.js
+++ client/src/utils/sortRecipes.js
@@ -4,9 +4,9 @@ export function sortRecipes (recipes, sortBy) {
   } else if (sortBy === 'Z-A') {
     return [...recipes].sort((a, b) => a.name<b.name ? 1 : -1);
   } else if (sortBy.includes('Best')) {
-
+    return [...recipes].sort((a, b) => a.rating<b.rating);
   } else if (sortBy.includes('Worst')) {
-
+    return [...recipes].sort((a, b) => a.rating>b.rating);
   }
   return recipes;
 }
