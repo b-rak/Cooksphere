39b83b30af6ef3c04014b16fa2e9de43bc3bea30
J7SUS <j7suspro@MacBook-Pro-JECM.local>
2025-01-16 16:22:13 +0100

    feat: Add Planned Changes App.js  comments

--- server/index.js
+++ server/index.js
@@ -11,5 +11,6 @@ app.use(express.json());
 app.use(router);
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT} 🙌✨`);
+  console.log("")
 });
