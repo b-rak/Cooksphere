d7111b7ce8ff4e5d299a4a0c7f7ae3a4ce048d93
Burak Tas <b.tas93@googlemail.com>
2025-02-20 15:43:38 +0100

    fix key prop error

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -1,23 +1,25 @@
 import { Link } from "react-router";
 
-export function RecipeList ({title, recipes}) {
+export function RecipeList({ title, recipes }) {
   return (
     <>
       <div className="px-8 py-4 bg-brown rounded-lg">
-        <h2 className='text-2xl font-bold mb-2 text-white'>{title}</h2>
-        <div className='flex gap-4 overflow-x-scroll scrollbar-none'>
-          {recipes.map(recipe => (
-            <>
-              <Link to={'/recipe/' + recipe._id} key={recipe._id}>
-                <div key={recipe._id} className='min-w-56 cursor-pointer bg-lightbeige rounded-lg shadow_2 p-1 h-full'>
-                  <img src={recipe.image} alt={recipe.name} className='w-56 h-56 rounded-lg'/>
-                  <div className='text-lg px-2'>{recipe.name}</div>
-                </div>
-              </Link>
-            </>)
-          )}
+        <h2 className="text-2xl font-bold mb-2 text-white">{title}</h2>
+        <div className="flex gap-4 overflow-x-scroll scrollbar-none">
+          {recipes.map((recipe) => (
+            <Link to={"/recipe/" + recipe._id} key={recipe._id}>
+              <div className="min-w-56 cursor-pointer bg-lightbeige rounded-lg shadow_2 p-1 h-full">
+                <img
+                  src={recipe.image}
+                  alt={recipe.name}
+                  className="w-56 h-56 rounded-lg"
+                />
+                <div className="text-lg px-2">{recipe.name}</div>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
     </>
   );
-};
+}
--- client/src/components/recipedetailspage/RecipeDetailsPage.jsx
+++ client/src/components/recipedetailspage/RecipeDetailsPage.jsx
@@ -1,32 +1,36 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
-import { getRecipe } from '../../ApiClient';
-import { GeneralCard } from './GeneralCard';
-import { Ingredients } from './Ingredients';
-import { Instructions } from './Instructions';
-import { Reviews } from './Reviews';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getRecipe } from "../../services/ApiClient";
+import { GeneralCard } from "./GeneralCard";
+import { Ingredients } from "./Ingredients";
+import { Instructions } from "./Instructions";
+import { Reviews } from "./Reviews";
 
-export function RecipeDetailsPage () {
+export function RecipeDetailsPage() {
   const { recipeId } = useParams();
   const [recipe, setRecipe] = useState(null);
   useEffect(() => {
     getRecipe(recipeId)
-      .then(data => setRecipe(data))
-      .catch(e => console.log(e));
+      .then((data) => setRecipe(data))
+      .catch((e) => console.log(e));
   }, [recipeId]);
 
   if (!recipe) {
-    return (<div>Loading...</div>)
+    return <div>Loading...</div>;
   }
 
   return (
     <>
-      <div className='py-16 px-24 w-full bg-lightbeige grid grid-rows-[repeat(5,auto)] md:grid-rows-[repeat(3,auto)] md:grid-cols-[22rem_1fr] gap-4'>
-        <GeneralCard recipe={recipe}/>
+      <div className="py-16 px-24 w-full bg-lightbeige grid grid-rows-[repeat(5,auto)] md:grid-rows-[repeat(3,auto)] md:grid-cols-[22rem_1fr] gap-4">
+        <GeneralCard recipe={recipe} />
         <Ingredients ingredients={recipe.ingredients} />
         <Instructions instructions={recipe.instructions} />
-        <Reviews reviews={recipe.reviews.filter(review => review.message.trim() !== '')}/>
+        <Reviews
+          reviews={recipe.reviews.filter(
+            (review) => review.message.trim() !== ""
+          )}
+        />
       </div>
     </>
   );
-};
+}
