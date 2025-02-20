a98200dc75d6b817d4925bf927ddcbc46b9b2187
Burak Tas <b.tas93@googlemail.com>
2025-02-20 15:45:51 +0100

    add profileicon component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,21 @@
-import { useContext } from "react";
-import { Link } from "react-router"
-import { AuthContext } from '../App';
+import { Link } from "react-router";
+import { ProfileIcon } from "./ProfileIcon";
 
-export function Navbar () {
-  const currentUser = useContext(AuthContext);
+export function Navbar() {
   return (
     <>
-      <header className='flex justify-between items-center px-8 py-2 shadow-[0_0_0.25rem_#808080]'>
-        <Link to='/'>
-          <img src="/logo.png" alt="Cooksphere Logo" className="w-16"/>
+      <header className="flex justify-between items-center px-8 py-2 shadow-[0_0_0.25rem_#808080]">
+        <Link to="/">
+          <img src="/logo.png" alt="Cooksphere Logo" className="w-16" />
         </Link>
-        <h1 className="flex-grow text-center font-fira font-black italic text-4xl">Cooksphere</h1>
+        <h1 className="flex-grow text-center font-fira font-black italic text-4xl">
+          Cooksphere
+        </h1>
         <nav>
           <a href=""></a>
         </nav>
-        <Link to='/profile'>
-          <img src={'/profile_' + currentUser.image +'.png'} alt="Profilepicture" className="w-16 rounded-full border-solid border-deepbrown border-2"/>
-        </Link>
+        <ProfileIcon />
       </header>
     </>
   );
-};
+}
---
+++ client/src/components/ProfileIcon.jsx
@@ -0,0 +1,93 @@
+import Cookies from "js-cookie";
+import { useEffect, useRef, useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { useAuthContext } from "../contexts/AuthContext";
+
+export function ProfileIcon() {
+  const { currentUser, setCurrentUser } = useAuthContext();
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuRef = useRef(null);
+  const iconRef = useRef(null);
+  const navigate = useNavigate();
+
+  async function logout() {
+    Cookies.remove("accessToken");
+    setOpenMenu(false);
+    setCurrentUser(null);
+    await navigate("/");
+  }
+
+  async function toProfile() {
+    setOpenMenu(false);
+    await navigate("/profile");
+  }
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target) &&
+        iconRef.current &&
+        !iconRef.current.contains(event.target)
+      ) {
+        setOpenMenu(false);
+      }
+    }
+
+    openMenu
+      ? document.addEventListener("mousedown", handleClickOutside)
+      : document.removeEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [openMenu]);
+
+  return (
+    <>
+      <div className="relative">
+        {currentUser ? (
+          <>
+            <img
+              src={"/profile_" + currentUser.image + ".png"}
+              ref={iconRef}
+              alt="Profilepicture"
+              className="w-16 rounded-full border-solid border-deepbrown border-2 cursor-pointer"
+              onClick={() => setOpenMenu(!openMenu)}
+            />
+            {openMenu ? (
+              <div
+                className="flex flex-col absolute right-0 top-20 w-40 bg-brown rounded-lg text-right text-lightbeige"
+                ref={menuRef}
+              >
+                <div className="p-2 font-medium">
+                  {currentUser.firstname + " " + currentUser.lastname}
+                </div>
+                <span
+                  onClick={toProfile}
+                  className="border-y border-y-softyellow w-full p-2 hover:bg-softyellow hover:text-deepbrown cursor-pointer"
+                >
+                  Mein Profil
+                </span>
+                <span
+                  className="cursor-pointer p-2 hover:bg-softyellow hover:text-deepbrown hover:rounded-b-lg"
+                  onClick={logout}
+                >
+                  Logout
+                </span>
+              </div>
+            ) : (
+              ""
+            )}
+          </>
+        ) : (
+          <Link to="/login">
+            <div className="w-16 text-right font-medium hover:underline font-fira">
+              LOGIN
+            </div>
+          </Link>
+        )}
+      </div>
+    </>
+  );
+}
