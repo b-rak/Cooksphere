ae8fdf837ea92213b8f2280884f64b546e7b453c
Burak Tas <b.tas93@googlemail.com>
2025-03-13 14:21:49 +0100

    server setup jest

---
+++ server/jest.setup.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+process.env.DATABASE_NAME = process.env.TEST_DATABASE_NAME || "test_cooksphere";
+
+import mongoose from "mongoose";
+import { connectDB, disconnectDB } from "./models";
+
+beforeAll(async () => {
+  await connectDB();
+});
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await disconnectDB();
+});
+
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany({});
+  }
+});
