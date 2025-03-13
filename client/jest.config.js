5b05536f550b1cd1ecc3e6d01427764c42e48ed2
Burak Tas <b.tas93@googlemail.com>
2025-03-13 12:28:36 +0100

    update jest config

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,11 @@
 export default {
-  collectCoverage: false,
+  collectCoverage: true,
   collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coveragePathIgnorePatterns: [
+    "src/tests/mocks/",
+    "src/config/",
+    "src/services/",
+  ],
   coverageDirectory: "coverage",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
